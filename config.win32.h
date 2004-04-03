19db12ca3a427cddc91bb1f5a0b62cc72baf3a15
Stef Walter <stef@thewalter.net>
2004-04-03 22:19:10 +0000

    Unix porting

--- config.win32.h
+++ config.win32.h
@@ -1,21 +1,7 @@
-/* Configure options for WIN32 */
+/* config.win32.h.  Hand edited for windows.  */
 
-#define MAX_BUFF	0x0F00000
-
-/* Define to one of `_getb67', `GETB67', `getb67' for Cray-2 and Cray-YMP
-   systems. This function is required for `alloca.c' support on those systems.
-   */
-/* #undef CRAY_STACKSEG_END */
-
-/* Define to 1 if using `alloca.c'. */
-/* #undef C_ALLOCA */
-
-/* Define to 1 if you have `alloca', as a function or macro. */
-#define HAVE_ALLOCA 1
-
-/* Define to 1 if you have <alloca.h> and it should be used (not on Ultrix).
-   */
-/* #undef HAVE_ALLOCA_H */
+/* Define to 1 if you have the <assert.h> header file. */
+#define HAVE_ASSERT_H 1
 
 /* Define to 1 if the system has the type `bool'. */
 /* #undef HAVE_BOOL */
@@ -23,39 +9,68 @@
 /* Define to 1 if the system has the type `byte'. */
 /* #undef HAVE_BYTE */
 
-/* Define to 1 if you have the `bzero' function. */
-/* #define HAVE_BZERO 1 */
-
 /* Define to 1 if you have the `chdir' function. */
-/* #define HAVE_CHDIR 1 */
+#define HAVE_CHDIR 1
 
-/* Define to 1 if you have the <dirent.h> header file, and it defines `DIR'.
-   */
-/* #define HAVE_DIRENT_H 1 */
+/* Define to 1 if you have the `chmod' function. */
+/* #define HAVE_CHMOD 1 */
 
-/* Define to 1 if you don't have `vprintf' but do have `_doprnt.' */
-/* #undef HAVE_DOPRNT */
+/* Define to 1 if you have the <errno.h> header file. */
+#define HAVE_ERRNO_H 1
 
 /* Define to 1 if you have the <err.h> header file. */
 /* #define HAVE_ERR_H 1 */
 
-/* Define to 1 if your system has a working `fnmatch' function. */
-/* #define HAVE_FNMATCH 1 */
+/* Define to 1 if you have the <fcntl.h> header file. */
+#define HAVE_FCNTL_H 1
 
 /* Define to 1 if you have the `getcwd' function. */
-/* #define HAVE_GETCWD 1 */
+#define HAVE_GETCWD 1
 
 /* Define to 1 if you have the `getopt' function. */
 /* #define HAVE_GETOPT 1 */
 
+/* Define to 1 if the system has the type `int16'. */
+/* #undef HAVE_INT16 */
+
+/* Define to 1 if the system has the type `int16_t'. */
+/* #define HAVE_INT16_T 1 */
+
+/* Define to 1 if the system has the type `int32'. */
+/* #undef HAVE_INT32 */
+
+/* Define to 1 if the system has the type `int32_t'. */
+/* #define HAVE_INT32_T 1 */
+
+/* Define to 1 if the system has the type `int64'. */
+/* #undef HAVE_INT64 */
+
+/* Define to 1 if the system has the type `int64_t'. */
+/* #define HAVE_INT64_T 1 */
+
 /* Define to 1 if you have the <inttypes.h> header file. */
 /* #define HAVE_INTTYPES_H 1 */
+
+/* Define to 1 if you have the <io.h> header file. */
+/* #undef HAVE_IO_H */
+
+/* Define to 1 if you have the `itoa' function. */
+/* #undef HAVE_ITOA */
+
+/* Define to 1 if you have the `itow' function. */
+/* #undef HAVE_ITOW */
+
+/* Define to 1 if you have the `lseek64' function. */
+/* #define HAVE_LSEEK64 1 */
 
 /* Define to 1 if you have the `malloc' function. */
 #define HAVE_MALLOC 1
 
-/* Define to 1 if you have the `memmove' function. */
-#define HAVE_MEMMOVE 1
+/* Define to 1 if you have the <malloc.h> header file. */
+#define HAVE_MALLOC_H 1
+
+/* Define to 1 if you have the `memcmp' function. */
+#define HAVE_MEMCMP 1
 
 /* Define to 1 if you have the <memory.h> header file. */
 #define HAVE_MEMORY_H 1
@@ -63,15 +78,17 @@
 /* Define to 1 if you have the `memset' function. */
 #define HAVE_MEMSET 1
 
-/* Define to 1 if you have the <ndir.h> header file, and it defines `DIR'. */
-/* #undef HAVE_NDIR_H */
+/* Define to 1 if you have the `realloc' function. */
+#define HAVE_REALLOC 1
+
+/* Define to 1 if you have the `reallocf' function. */
+/* #undef HAVE_REALLOCF */
+
+/* Define to 1 if you have the `sprintf' function. */
+#define HAVE_SPRINTF 1
 
 /* Define to 1 if you have the `stat' function. */
-/* #define HAVE_STAT 1 */
-
-/* Define to 1 if `stat' has the bug that it succeeds when given the
-   zero-length file name argument. */
-/* #undef HAVE_STAT_EMPTY_STRING_BUG */
+#define HAVE_STAT 1
 
 /* Define to 1 if you have the <stdarg.h> header file. */
 #define HAVE_STDARG_H 1
@@ -80,34 +97,16 @@
 #define HAVE_STDDEF_H 1
 
 /* Define to 1 if you have the <stdint.h> header file. */
-/* #undef HAVE_STDINT_H */
+/* #define HAVE_STDINT_H 1 */
+
+/* Define to 1 if you have the <stdio.h> header file. */
+#define HAVE_STDIO_H 1
 
 /* Define to 1 if you have the <stdlib.h> header file. */
 #define HAVE_STDLIB_H 1
 
-/* Define to 1 if you have the `strcasecmp' function. */
-/* #define HAVE_STRCASECMP 1 */
-
-/* Define to 1 if you have the `strcasecmp' function. */
-#define HAVE_STRICMP 1
-
-/* Define to 1 if you have the `strcasestr' function. */
-/* #define HAVE_STRCASESTR 1 */
-
 /* Define to 1 if you have the `strchr' function. */
 #define HAVE_STRCHR 1
-
-/* Define to 1 if you have the `strcpy' function. */
-#define HAVE_STRCPY 1
-
-/* Define to 1 if you have the `strcat' function. */
-#define HAVE_STRCAT 1
-
-/* Define to 1 if you have the `strcspn' function. */
-#define HAVE_STRCSPN 1
-
-/* Define to 1 if you have the `strdup' function. */
-#define HAVE_STRDUP 1
 
 /* Define to 1 if you have the `strerror' function. */
 #define HAVE_STRERROR 1
@@ -118,43 +117,11 @@
 /* Define to 1 if you have the <string.h> header file. */
 #define HAVE_STRING_H 1
 
-/* Define to 1 if you have the `strlcpy' function. */
-/* #define HAVE_STRLCPY 1 */
-
-/* Define to 1 if you have the `strncpy' function. */
-#define HAVE_STRNCPY 1
-
-/* Define to 1 if you have the `strlcat' function. */
-/* #define HAVE_STRLCAT 1 */
-
-/* Define to 1 if you have the `strncat' function. */
-#define HAVE_STRNCAT 1
-
-/* Define to 1 if you have the `strncasecmp' function. */
-/* #define HAVE_STRNCASECMP 1 */
-
-/* Define to 1 if you have the `strncasecmp' function. */
-#define HAVE_STRNICMP 1
-
-/* Define to 1 if you have the `strndup' function. */
-/* #undef HAVE_STRNDUP */
-
-/* Define to 1 if you have the `strrchr' function. */
-#define HAVE_STRRCHR 1
-
-/* Define to 1 if you have the `strspn' function. */
-#define HAVE_STRSPN 1
-
-/* Define to 1 if you have the <sys/dir.h> header file, and it defines `DIR'.
-   */
-/* #undef HAVE_SYS_DIR_H */
-
-/* Define to 1 if you have the <sys/ndir.h> header file, and it defines `DIR'.
-   */
-/* #undef HAVE_SYS_NDIR_H */
-
 /* Define to 1 if you have the <sys/stat.h> header file. */
-#define HAVE_SYS_STAT_H 1
+/* #define HAVE_SYS_STAT_H 1 */
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+/* #define HAVE_SYS_TIME_H 1 */
 
 /* Define to 1 if you have the <sys/types.h> header file. */
 #define HAVE_SYS_TYPES_H 1
@@ -162,53 +129,71 @@
 /* Define to 1 if the system has the type `uint'. */
 /* #define HAVE_UINT 1 */
 
+/* Define to 1 if the system has the type `uint16'. */
+/* #undef HAVE_UINT16 */
+
+/* Define to 1 if the system has the type `uint16_t'. */
+/* #define HAVE_UINT16_T 1 */
+
+/* Define to 1 if the system has the type `uint32'. */
+/* #undef HAVE_UINT32 */
+
+/* Define to 1 if the system has the type `uint32_t'. */
+/* #define HAVE_UINT32_T 1 */
+
+/* Define to 1 if the system has the type `uint64'. */
+/* #undef HAVE_UINT64 */
+
+/* Define to 1 if the system has the type `uint64_t'. */
+/* #define HAVE_UINT64_T 1 */
+
 /* Define to 1 if you have the <unistd.h> header file. */
-/* #define HAVE_UNISTD_H 1 */
+#define HAVE_UNISTD_H 1
 
-/* Define to 1 if you have the `vasprintf' function. */
-/* #define HAVE_VASPRINTF 1 */
+/* Define to 1 if you have the `utimes' function. */
+/* #define HAVE_UTIMES 1 */
 
-/* Define to 1 if you have the `vprintf' function. */
-#define HAVE_VPRINTF 1
+/* Define to 1 if you have the <wchar.h> header file. */
+#define HAVE_WCHAR_H 1
 
-/* Define to 1 if you have the `vsnprintf' function. */
-/* #define HAVE_VSNPRINTF 1 */
+/* Define to 1 if you have the `wchdir' function. */
+/* #undef HAVE_WCHDIR */
 
-/* Define to 1 if `lstat' dereferences a symlink specified with a trailing
-   slash. */
-/* #undef LSTAT_FOLLOWS_SLASHED_SYMLINK */
+/* Define to 1 if you have the `wmkdir' function. */
+/* #undef HAVE_WMKDIR */
+
+/* Define to 1 if you have the `wopen' function. */
+/* #undef HAVE_WOPEN */
 
 /* Name of package */
-#define PACKAGE "rep"
+#define PACKAGE "scrounge-ntfs"
 
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT "nielsen@memberwebs.com"
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "rep"
+#define PACKAGE_NAME "scrounge-ntfs"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "rep 2.1.2b"
+#define PACKAGE_STRING "scrounge-ntfs 0.8.1"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "rep"
+#define PACKAGE_TARNAME "scrounge-ntfs"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.1.2b"
+#define PACKAGE_VERSION "0.8.1"
 
-/* If using the C implementation of alloca, define if you know the
-   direction of stack growth for your system; otherwise it will be
-   automatically deduced at run-time.
-        STACK_DIRECTION > 0 => grows toward higher addresses
-        STACK_DIRECTION < 0 => grows toward lower addresses
-        STACK_DIRECTION = 0 => direction of growth unknown */
-/* #undef STACK_DIRECTION */
+/* The size of a `off_t', as computed by sizeof. */
+#define SIZEOF_OFF_T 4
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
+/* Define to 1 if you can safely include both <sys/time.h> and <time.h>. */
+/* #define TIME_WITH_SYS_TIME 1 */
+
 /* Version number of package */
-#define VERSION "2.1.1b"
+#define VERSION "0.8.1"
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
