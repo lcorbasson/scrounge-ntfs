28173f3e2037a37cdd6d6afe7f1b084e216df1ca
Stef Walter <stef@thewalter.net>
2010-10-18 19:59:21 +0000

    A few changes to the project after move to git.

---
+++ autogen.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+# Some boiler plate to get git setup as expected
+if test -d .git; then
+	if test -f .git/hooks/pre-commit.sample && \
+	   test ! -f .git/hooks/pre-commit; then
+		cp -pv .git/hooks/pre-commit.sample .git/hooks/pre-commit
+	fi
+fi
+
+set -x
+
+aclocal
+autoheader
+automake -a
+autoconf
+./configure "$@"
